cf0eead529afdf96919ff2943a7a84e2c6850cd2
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2020-03-11 00:24:08 -0500

    set up migration with dummy data

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,15 @@ namespace Persistence
         }
 
         public DbSet<Value>  Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "First Value"},
+                    new Value { Id = 2, Name = "Second Value"},
+                    new Value { Id = 3, Name = "Third Value"}
+                );
+        }
     }
 }
